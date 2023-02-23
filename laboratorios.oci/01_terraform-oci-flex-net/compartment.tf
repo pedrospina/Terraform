794f38fb83f8ee3aa8ee0271d117b9f32f73449b
Pedro Ospina <daniel.ospina12@gmail.com>
2023-02-23 11:26:57 -0500

    Borrar laboratorio

--- laboratorios.oci/01_terraform-oci-flex-net/compartment.tf
+++
@@ -1,5 +0,0 @@
-resource "oci_identity_compartment" "Prueba" {
-  name           = "Pruebas_OCI"
-  description    = "CCC Compartment"
-  compartment_id = var.compartment_ocid
-}
--- laboratorios.oci/01_terraform-oci-flex-net/nat_gateway.tf
+++
@@ -1,5 +0,0 @@
-resource "oci_core_nat_gateway" "NATGateway_prueba" {
-  compartment_id = oci_identity_compartment.CCCCompartment.id
-  display_name   = "NATGateway_prueba"
-  vcn_id         = oci_core_virtual_network.CCCVCN.id
-}
